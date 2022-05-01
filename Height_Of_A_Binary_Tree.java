92bc313e7a6a598adc75aed219e7b4c32dc193b1
Ritu Raj Anand <riturajanand@Ritus-MacBook-Air.local>
2022-05-01 10:47:07 +0530

    Height of a binary Tree

---
+++ Height_Of_A_Binary_Tree.java
@@ -0,0 +1,35 @@
+
+public class Height_Of_A_Binary_Tree 
+{
+	//Classical solution
+	static int height;
+     public static int find_height(Node root)
+     {
+    	 if(root==null)
+    		 return 0;
+    	 
+    	 int left_height=find_height(root.left);
+    	 int right_height=find_height(root.right);
+    	 
+    	 return Math.max(left_height, right_height)+1;
+
+     }
+     //Static optimisation
+     public static int find_ht(Node root)
+     {
+    	 height=0;
+    	 helper(root,0);
+    	 return height;
+     }
+     public static void helper(Node root,int val)
+     {
+    	 if(root==null)
+    	 {
+    		 height=Math.max(height, val);
+    		 return;
+    	 }
+    	 
+    	 helper(root.left,val+1);
+    	 helper(root.right,val+1);
+     }
+}
